f0b632f19c48e6ac555d04ce9750f13ee3220bdc
OlgaPerolainen <95132545+OlgaPerolainen@users.noreply.github.com>
2022-03-10 16:07:13 +0300

    Add files via upload

---
+++ Lab09 EntityFramework/CustomerManager1/CustomerViewer.Designer.cs
@@ -0,0 +1,355 @@
+﻿
+namespace CustomerManager1
+{
+    partial class CustomerViewer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customerList = new System.Windows.Forms.ComboBox();
+            this.listBoxOrders = new System.Windows.Forms.ListBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblRhoto = new System.Windows.Forms.Label();
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.txtBoxLastName = new System.Windows.Forms.TextBox();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
+            this.txtBoxAge = new System.Windows.Forms.TextBox();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.btnAddData = new System.Windows.Forms.Button();
+            this.btnShowData = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtnOrders = new System.Windows.Forms.RadioButton();
+            this.rBtnCustomers = new System.Windows.Forms.RadioButton();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtBoxID = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.rbtnVIPOrders = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // customerList
+            // 
+            this.customerList.FormattingEnabled = true;
+            this.customerList.Location = new System.Drawing.Point(11, 13);
+            this.customerList.Name = "customerList";
+            this.customerList.Size = new System.Drawing.Size(197, 24);
+            this.customerList.TabIndex = 0;
+            // 
+            // listBoxOrders
+            // 
+            this.listBoxOrders.FormattingEnabled = true;
+            this.listBoxOrders.ItemHeight = 16;
+            this.listBoxOrders.Location = new System.Drawing.Point(12, 44);
+            this.listBoxOrders.Name = "listBoxOrders";
+            this.listBoxOrders.Size = new System.Drawing.Size(197, 148);
+            this.listBoxOrders.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(232, 16);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 17);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Имя";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(232, 44);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(70, 17);
+            this.lblLastName.TabIndex = 3;
+            this.lblLastName.Text = "Фамилия";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(232, 72);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(42, 17);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(232, 102);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(62, 17);
+            this.lblAge.TabIndex = 5;
+            this.lblAge.Text = "Возраст";
+            // 
+            // lblRhoto
+            // 
+            this.lblRhoto.AutoSize = true;
+            this.lblRhoto.Location = new System.Drawing.Point(232, 158);
+            this.lblRhoto.Name = "lblRhoto";
+            this.lblRhoto.Size = new System.Drawing.Size(44, 17);
+            this.lblRhoto.TabIndex = 6;
+            this.lblRhoto.Text = "Фото";
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBoxName.Location = new System.Drawing.Point(308, 13);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(134, 22);
+            this.txtBoxName.TabIndex = 7;
+            // 
+            // txtBoxLastName
+            // 
+            this.txtBoxLastName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBoxLastName.Location = new System.Drawing.Point(308, 41);
+            this.txtBoxLastName.Name = "txtBoxLastName";
+            this.txtBoxLastName.Size = new System.Drawing.Size(134, 22);
+            this.txtBoxLastName.TabIndex = 8;
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBoxEmail.Location = new System.Drawing.Point(308, 69);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(134, 22);
+            this.txtBoxEmail.TabIndex = 9;
+            // 
+            // txtBoxAge
+            // 
+            this.txtBoxAge.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBoxAge.Location = new System.Drawing.Point(308, 99);
+            this.txtBoxAge.Name = "txtBoxAge";
+            this.txtBoxAge.Size = new System.Drawing.Size(134, 22);
+            this.txtBoxAge.TabIndex = 10;
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChooseFile.Location = new System.Drawing.Point(308, 141);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(134, 51);
+            this.btnChooseFile.TabIndex = 11;
+            this.btnChooseFile.Text = "Выбрать файлы";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // btnAddData
+            // 
+            this.btnAddData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddData.Location = new System.Drawing.Point(472, 13);
+            this.btnAddData.Name = "btnAddData";
+            this.btnAddData.Size = new System.Drawing.Size(117, 73);
+            this.btnAddData.TabIndex = 12;
+            this.btnAddData.Text = "Добавить данные";
+            this.btnAddData.UseVisualStyleBackColor = true;
+            this.btnAddData.Click += new System.EventHandler(this.btnAddData_Click);
+            // 
+            // btnShowData
+            // 
+            this.btnShowData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnShowData.Location = new System.Drawing.Point(595, 13);
+            this.btnShowData.Name = "btnShowData";
+            this.btnShowData.Size = new System.Drawing.Size(129, 73);
+            this.btnShowData.TabIndex = 13;
+            this.btnShowData.Text = "Показать данные";
+            this.btnShowData.UseVisualStyleBackColor = true;
+            this.btnShowData.Click += new System.EventHandler(this.btnShowData_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdate.Location = new System.Drawing.Point(729, 13);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(127, 33);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Редактировать";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Location = new System.Drawing.Point(729, 53);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(127, 33);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.rbtnVIPOrders);
+            this.groupBox1.Controls.Add(this.rbtnOrders);
+            this.groupBox1.Controls.Add(this.rBtnCustomers);
+            this.groupBox1.Location = new System.Drawing.Point(472, 99);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(384, 57);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор данных";
+            // 
+            // rbtnOrders
+            // 
+            this.rbtnOrders.AutoSize = true;
+            this.rbtnOrders.Location = new System.Drawing.Point(138, 28);
+            this.rbtnOrders.Name = "rbtnOrders";
+            this.rbtnOrders.Size = new System.Drawing.Size(78, 21);
+            this.rbtnOrders.TabIndex = 1;
+            this.rbtnOrders.TabStop = true;
+            this.rbtnOrders.Text = "Заказы";
+            this.rbtnOrders.UseVisualStyleBackColor = true;
+            // 
+            // rBtnCustomers
+            // 
+            this.rBtnCustomers.AutoSize = true;
+            this.rBtnCustomers.Location = new System.Drawing.Point(20, 28);
+            this.rBtnCustomers.Name = "rBtnCustomers";
+            this.rBtnCustomers.Size = new System.Drawing.Size(87, 21);
+            this.rBtnCustomers.TabIndex = 0;
+            this.rBtnCustomers.TabStop = true;
+            this.rBtnCustomers.Text = "Клиенты";
+            this.rBtnCustomers.UseVisualStyleBackColor = true;
+            // 
+            // lblID
+            // 
+            this.lblID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(469, 173);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(21, 17);
+            this.lblID.TabIndex = 17;
+            this.lblID.Text = "ID";
+            // 
+            // txtBoxID
+            // 
+            this.txtBoxID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBoxID.Location = new System.Drawing.Point(515, 170);
+            this.txtBoxID.Name = "txtBoxID";
+            this.txtBoxID.Size = new System.Drawing.Size(341, 22);
+            this.txtBoxID.TabIndex = 18;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-5, 198);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(875, 259);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // rbtnVIPOrders
+            // 
+            this.rbtnVIPOrders.AutoSize = true;
+            this.rbtnVIPOrders.Location = new System.Drawing.Point(245, 28);
+            this.rbtnVIPOrders.Name = "rbtnVIPOrders";
+            this.rbtnVIPOrders.Size = new System.Drawing.Size(132, 21);
+            this.rbtnVIPOrders.TabIndex = 2;
+            this.rbtnVIPOrders.TabStop = true;
+            this.rbtnVIPOrders.Text = "Важные заказы";
+            this.rbtnVIPOrders.UseVisualStyleBackColor = true;
+            // 
+            // CustomerViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtBoxID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnShowData);
+            this.Controls.Add(this.btnAddData);
+            this.Controls.Add(this.btnChooseFile);
+            this.Controls.Add(this.txtBoxAge);
+            this.Controls.Add(this.txtBoxEmail);
+            this.Controls.Add(this.txtBoxLastName);
+            this.Controls.Add(this.txtBoxName);
+            this.Controls.Add(this.lblRhoto);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.listBoxOrders);
+            this.Controls.Add(this.customerList);
+            this.MinimumSize = new System.Drawing.Size(886, 497);
+            this.Name = "CustomerViewer";
+            this.Text = "Customer Viewer";
+            this.Load += new System.EventHandler(this.CustomerViewer_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox customerList;
+        private System.Windows.Forms.ListBox listBoxOrders;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblRhoto;
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.TextBox txtBoxLastName;
+        private System.Windows.Forms.TextBox txtBoxEmail;
+        private System.Windows.Forms.TextBox txtBoxAge;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.Button btnAddData;
+        private System.Windows.Forms.Button btnShowData;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtnOrders;
+        private System.Windows.Forms.RadioButton rBtnCustomers;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtBoxID;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton rbtnVIPOrders;
+    }
+}
+
---
+++ Lab09 EntityFramework/CustomerManager1/CustomerViewer.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using CodeFirst;
+
+namespace CustomerManager1
+{
+    public partial class CustomerViewer : Form
+    {
+        SampleContext context = new SampleContext();
+        byte[] Ph;
+        public CustomerViewer()
+        {
+            InitializeComponent();
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<SampleContext>());
+        }
+
+        private void btnAddData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Customer customer = new Customer
+                {
+                    FirstName = this.txtBoxName.Text,
+                    LastName = this.txtBoxLastName.Text,
+                    Email = this.txtBoxEmail.Text,
+                    Age = Int32.Parse(this.txtBoxAge.Text),
+                    Photo = Ph,
+                    Orders = listBoxOrders.SelectedItems.OfType<Order>().ToList()
+                };
+                context.Customers.Add(customer);
+                context.SaveChanges();
+                Output();
+                ClearAll();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+        private void Output()
+        {
+            if (this.rBtnCustomers.Checked == true)
+                dataGridView1.DataSource = context.Customers.ToList();
+            else if (this.rbtnOrders.Checked == true)
+                dataGridView1.DataSource = context.Orders.ToList();
+            else if (this.rbtnVIPOrders.Checked == true)
+                dataGridView1.DataSource = context.VipOrders.ToList();
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Image bm = new Bitmap(dialog.OpenFile());
+                ImageConverter converter = new ImageConverter();
+                Ph = (byte[])converter.ConvertTo(bm, typeof(byte[]));
+            }
+        }
+
+        private void btnShowData_Click(object sender, EventArgs e)
+        {
+            Output();
+            var query = from b in context.Customers
+                        orderby b.FirstName
+                        select b;
+            customerList.DataSource = query.ToList();
+        }
+
+        private void CustomerViewer_Load(object sender, EventArgs e)
+        {
+            context.Orders.Add(new Order { ProductName = "Аудио", Quantity = 12, PurchaseDate = DateTime.Parse("12.01.2016") });
+            context.Orders.Add(new Order { ProductName = "Видео", Quantity = 22, PurchaseDate = DateTime.Parse("10.01.2016") });
+            context.VipOrders.Add(new VipOrder { ProductName = "Авто", Quantity = 101, PurchaseDate = DateTime.Parse("10.01.2016"), status = "Высокий" });
+            context.SaveChanges();
+
+            listBoxOrders.DataSource = context.Orders.ToList();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null) return;
+            var customer = dataGridView1.CurrentRow.DataBoundItem as Customer;
+            if (customer == null) return;
+            lblID.Text = Convert.ToString(customer.CustomerID);
+            txtBoxID.Text = customer.ToString();
+            txtBoxName.Text = customer.FirstName;
+            txtBoxLastName.Text = customer.LastName;
+            txtBoxEmail.Text = customer.Email;
+            txtBoxAge.Text = customer.Age.ToString();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (lblID.Text == String.Empty) return;
+
+            var id = Convert.ToInt32(lblID.Text);
+            var customer = context.Customers.Find(id);
+            if (customer == null) return;
+
+            customer.FirstName = this.txtBoxName.Text;
+            customer.LastName = this.txtBoxLastName.Text;
+            customer.Email = this.txtBoxEmail.Text;
+            customer.Age = Int32.Parse(this.txtBoxAge.Text);
+
+            context.Entry(customer).State = EntityState.Modified;
+            context.SaveChanges();
+            Output();
+            ClearAll();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lblID.Text == String.Empty) return;
+            var id = Convert.ToInt32(lblID.Text);
+            var customer = context.Customers.Find(id);
+
+            context.Entry(customer).State = EntityState.Deleted;
+            context.SaveChanges();
+            Output();
+            ClearAll();
+        }
+
+        private void ClearAll()
+        {
+            txtBoxName.Text = String.Empty;
+            txtBoxLastName.Text = String.Empty;
+            txtBoxEmail.Text = String.Empty;
+            txtBoxAge.Text = String.Empty;
+            lblID.Text = "ID";
+            txtBoxID.Text = String.Empty;
+            customerList.Text = String.Empty;
+        }
+    }
+}
