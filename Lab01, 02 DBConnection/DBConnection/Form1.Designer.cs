3910373b3a07c811ca76892ba8c43e5f594d8147
OlgaPerolainen <95132545+OlgaPerolainen@users.noreply.github.com>
2022-03-10 15:48:54 +0300

    Add files via upload

---
+++ Lab01, 02 DBConnection/DBConnection/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.DBConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectionListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.DBConnectionToolStripMenuItem,
+            this.stopConnectionToolStripMenuItem,
+            this.connectionListToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // DBConnectionToolStripMenuItem
+            // 
+            this.DBConnectionToolStripMenuItem.Name = "DBConnectionToolStripMenuItem";
+            this.DBConnectionToolStripMenuItem.Size = new System.Drawing.Size(227, 24);
+            this.DBConnectionToolStripMenuItem.Text = "&Подключиться к базе данных";
+            this.DBConnectionToolStripMenuItem.Click += new System.EventHandler(this.DBConnectionToolStripMenuItem_Click);
+            // 
+            // stopConnectionToolStripMenuItem
+            // 
+            this.stopConnectionToolStripMenuItem.Name = "stopConnectionToolStripMenuItem";
+            this.stopConnectionToolStripMenuItem.Size = new System.Drawing.Size(178, 24);
+            this.stopConnectionToolStripMenuItem.Text = "&Прервать соединение";
+            this.stopConnectionToolStripMenuItem.Click += new System.EventHandler(this.stopConnectionToolStripMenuItem_Click);
+            // 
+            // connectionListToolStripMenuItem
+            // 
+            this.connectionListToolStripMenuItem.Name = "connectionListToolStripMenuItem";
+            this.connectionListToolStripMenuItem.Size = new System.Drawing.Size(173, 24);
+            this.connectionListToolStripMenuItem.Text = "&Список подключений";
+            this.connectionListToolStripMenuItem.Click += new System.EventHandler(this.connectionListToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Количество товаров";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(444, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество товаров";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(64, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Список товаров";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(444, 159);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(180, 170);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название товара";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(64, 231);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 44);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Транзакция";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem DBConnectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopConnectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectionListToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
