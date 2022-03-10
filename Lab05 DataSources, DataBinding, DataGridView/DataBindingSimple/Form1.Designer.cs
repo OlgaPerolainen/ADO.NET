b1fc22d803b5b8e8b874275753fd4f3d9b51728e
OlgaPerolainen <95132545+OlgaPerolainen@users.noreply.github.com>
2022-03-10 15:58:55 +0300

    Add files via upload

---
+++ Lab05 DataSources, DataBinding, DataGridView/DataBindingSimple/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace DataBindingSimple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.northwindDataSet1 = new DataBindingSimple.NorthwindDataSet();
+            this.productsTableAdapter1 = new DataBindingSimple.NorthwindDataSetTableAdapters.ProductsTableAdapter();
+            this.txtBoxProductID = new System.Windows.Forms.TextBox();
+            this.txtBoxProductName = new System.Windows.Forms.TextBox();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnMoveFirst = new System.Windows.Forms.Button();
+            this.btnMoveLast = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // northwindDataSet1
+            // 
+            this.northwindDataSet1.DataSetName = "NorthwindDataSet";
+            this.northwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productsTableAdapter1
+            // 
+            this.productsTableAdapter1.ClearBeforeFill = true;
+            // 
+            // txtBoxProductID
+            // 
+            this.txtBoxProductID.Location = new System.Drawing.Point(81, 62);
+            this.txtBoxProductID.Name = "txtBoxProductID";
+            this.txtBoxProductID.Size = new System.Drawing.Size(424, 22);
+            this.txtBoxProductID.TabIndex = 0;
+            this.txtBoxProductID.TextChanged += new System.EventHandler(this.txtBoxProductID_TextChanged);
+            // 
+            // txtBoxProductName
+            // 
+            this.txtBoxProductName.Location = new System.Drawing.Point(81, 126);
+            this.txtBoxProductName.Name = "txtBoxProductName";
+            this.txtBoxProductName.Size = new System.Drawing.Size(424, 22);
+            this.txtBoxProductName.TabIndex = 1;
+            this.txtBoxProductName.TextChanged += new System.EventHandler(this.txtBoxProductName_TextChanged);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(185, 289);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(87, 24);
+            this.btnPrevious.TabIndex = 2;
+            this.btnPrevious.Text = "<";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(312, 289);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(87, 24);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnMoveFirst
+            // 
+            this.btnMoveFirst.Location = new System.Drawing.Point(81, 289);
+            this.btnMoveFirst.Name = "btnMoveFirst";
+            this.btnMoveFirst.Size = new System.Drawing.Size(87, 24);
+            this.btnMoveFirst.TabIndex = 4;
+            this.btnMoveFirst.Text = "<<";
+            this.btnMoveFirst.UseVisualStyleBackColor = true;
+            this.btnMoveFirst.Click += new System.EventHandler(this.btnMoveFirst_Click);
+            // 
+            // btnMoveLast
+            // 
+            this.btnMoveLast.Location = new System.Drawing.Point(418, 289);
+            this.btnMoveLast.Name = "btnMoveLast";
+            this.btnMoveLast.Size = new System.Drawing.Size(87, 24);
+            this.btnMoveLast.TabIndex = 5;
+            this.btnMoveLast.Text = ">>";
+            this.btnMoveLast.UseVisualStyleBackColor = true;
+            this.btnMoveLast.Click += new System.EventHandler(this.btnMoveLast_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 383);
+            this.Controls.Add(this.btnMoveLast);
+            this.Controls.Add(this.btnMoveFirst);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.txtBoxProductName);
+            this.Controls.Add(this.txtBoxProductID);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Products";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NorthwindDataSet northwindDataSet1;
+        private NorthwindDataSetTableAdapters.ProductsTableAdapter productsTableAdapter1;
+        private System.Windows.Forms.TextBox txtBoxProductID;
+        private System.Windows.Forms.TextBox txtBoxProductName;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnMoveFirst;
+        private System.Windows.Forms.Button btnMoveLast;
+    }
+}
+
---
+++ Lab05 DataSources, DataBinding, DataGridView/DataBindingSimple/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBindingSimple
+{
+    public partial class Form1 : Form
+    {
+        private BindingSource productBindingSource;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            productsTableAdapter1.Fill(northwindDataSet1.Products);
+            productBindingSource = new BindingSource(northwindDataSet1, "Products");
+            txtBoxProductID.DataBindings.Add("Text", productBindingSource, "ProductID");
+            txtBoxProductName.DataBindings.Add("Text", productBindingSource, "ProductName");
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            productBindingSource.MovePrevious();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            productBindingSource.MoveNext();
+        }
+
+        private void btnMoveLast_Click(object sender, EventArgs e)
+        {
+            productBindingSource.MoveLast();
+        }
+
+        private void txtBoxProductID_TextChanged(object sender, EventArgs e)
+        {
+            if (productBindingSource.Position == 0)
+            {
+                btnMoveFirst.Enabled = false;
+                btnPrevious.Enabled = false;
+                btnMoveLast.Enabled = true;
+                btnNext.Enabled = true;
+                btnNext.Focus();
+            }
+            else if(productBindingSource.Position + 1 == productBindingSource.Count)
+            {
+                btnMoveLast.Enabled = false;
+                btnNext.Enabled = false;
+                btnMoveFirst.Enabled = true;
+                btnPrevious.Enabled = true;
+                btnPrevious.Focus();
+            }
+            else
+            {
+                btnMoveFirst.Enabled = true;
+                btnPrevious.Enabled = true;
+                btnMoveLast.Enabled = true;
+                btnNext.Enabled = true;
+            }
+        }
+
+        private void txtBoxProductName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMoveFirst_Click(object sender, EventArgs e)
+        {
+            productBindingSource.MoveFirst();
+        }
+    }
+}
